1c8633ae98b15dd625da8145540b83c65b1e229b
Adilson Rodrigues da Silva <adilsons>
2020-05-31 17:21:06 -0300

    Alteração request mapping

--- src/main/java/br/com/account_launch/controller/AccountLaunchController.java
+++ src/main/java/br/com/account_launch/controller/AccountLaunchController.java
@@ -1,12 +1,14 @@
 package br.com.account_launch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/v1/accounts-launch")
 public class AccountLaunchController {
 
-	@GetMapping("/")
+	@GetMapping
 	public String teste() {
 		return "<h2> Adilson Silva Docker </h2>";
 	}
